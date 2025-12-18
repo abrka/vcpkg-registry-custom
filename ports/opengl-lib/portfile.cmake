13e39e987edf0fcb77fd8d4435d6b3cc36d5317f
abrka <ifradnashit@gmail.com>
2025-12-18 20:01:46 +0600

    fix

--- ports/opengl-lib/portfile.cmake
+++ ports/opengl-lib/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO abrka/opengl_lib
     REF 0d0cfc4
-    SHA512 0
+    SHA512 a117e3aaae20246ccfb323ecc8050bb2e243d60cbd4c1f37d6105b91e2a35427f5508f639c986855bbcf5ed2f30acd6209c98e2f81ad00b3a403dcf151628dba
     HEAD_REF master
 )
 
