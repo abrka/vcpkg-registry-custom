07632a3d8280edc413bb766a7daeee9e4cfd521a
abrka <ifradnashit@gmail.com>
2025-12-18 19:21:26 +0600

    first commit

---
+++ ports/opengl-lib/portfile.cmake
@@ -0,0 +1,11 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO abrka/opengl_lib
+    REF 3d5b3e6
+    SHA512 d9d297818f2605164cffcec1154cf0a62e17140091ace592faf834d6f689768cc64ffbda0e196da0b9589623d556b084914c1aefbd72cbff5174955917946b44
+    HEAD_REF master
+)
+
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
+vcpkg_cmake_install()
+vcpkg_install_copyright("${SOURCE_PATH}/LICENSE")
